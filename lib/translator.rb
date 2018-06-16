5f28bbb2cf529e285546febe4aeb5858e1c0604e
Jason Pitts <juniorjr083@gmail.com>
2018-06-16 15:19:00 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,33 @@
 # require modules here
+require "yaml"
 
-def load_library
+
+def load_library(file)
   # code goes here
+  new_hash = {}
+  a = YAML.load_file("#{file}")
+  new_hash["get_meaning"] = {}
+  new_hash["get_emoticon"] = {}
+  #puts a
+  a.each do |k,v|
+  new_hash["get_meaning"]["#{v.last}"] = "#{k}"
+    # puts ele.class
+    new_hash["get_emoticon"]["#{v.first}"] = "#{v.last}"
+ 
+  
+  
+     
+  
+end
+ 
+ new_hash
+  
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file,emoticon)
   # code goes here
+  a = load_library(file)
+  a["get_emoticon"].include?(emoticon) ? a["get_emoticon"]["#{emoticon}"] : a 
 end
 
 def get_english_meaning
